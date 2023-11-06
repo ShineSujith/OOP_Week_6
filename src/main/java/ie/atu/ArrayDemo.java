40b247e9821c4ffbbf82c3f90aa2926f0ce8c31e
Shine Sujith <G00414454@atu.ie>
2023-11-06 16:39:41 +0000

    Array anArray was created

--- src/main/java/ie/atu/ArrayDemo.java
+++ src/main/java/ie/atu/ArrayDemo.java
@@ -1,4 +1,24 @@
 package ie.atu;
 
+import java.util.Scanner;
 public class ArrayDemo {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int[] anArray;
+
+        anArray = new int[10];
+
+        anArray[0] = 100;
+        anArray[1] = 200;
+        anArray[2] = 300;
+        anArray[3] = 400;
+        anArray[4] = 500;
+        anArray[5] = 600;
+        anArray[6] = 700;
+        anArray[7] = 800;
+        anArray[8] = 900;
+        anArray[9] = 1000;
+
+    }
 }
